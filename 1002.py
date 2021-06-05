f95da6366efd8ba53931a21cf3ac44b2a7bea9c7
Arpit Mathur <20039537+arpitmathur1@users.noreply.github.com>
2021-06-05 13:49:18 +1000

    submitted 1002
    Runtime: 60 ms, faster than 41.98% of Python online submissions for Find Common Characters.
    Memory Usage: 13.8 MB, less than 15.78% of Python online submissions for Find Common Characters.

--- 1002.py
+++ 1002.py
@@ -1,2 +1,24 @@
 # Find Common Characters
+import collections
 
+class Solution(object):
+    def commonChars(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        res = collections.Counter(words[0])
+        for a in words:
+            res &= collections.Counter(a)
+        return list(res.elements())
+
+
+
+
+s = Solution()
+print(s.commonChars(["bella","label","roller"]))
+
+"""
+Runtime: 60 ms, faster than 41.98% of Python online submissions for Find Common Characters.
+Memory Usage: 13.8 MB, less than 15.78% of Python online submissions for Find Common Characters.
+"""
